5f142dde78a694a65666b0960e10d1cdb6875894
semanticdata <contact@miguelpimentel.do>
2025-10-16 10:16:48 -0500

    add and configure eslint for json

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
-import js from "@eslint/js";
-import globals from "globals";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import json from "@eslint/json";
 
 export default defineConfig([
+  // lint JS files
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
@@ -10,6 +12,28 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser
     },
+  },
+  // lint JSON files
+  {
+    files: ["**/*.json"],
+    ignores: ["package-lock.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  // lint JSONC files
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  // lint JSON5 files
+  {
+    files: ["**/*.json5"],
+    plugins: { json },
+    language: "json/json5",
+    extends: ["json/recommended"],
   },
   // Project-level override: enable Node env for Eleventy and other build/config files
   {
