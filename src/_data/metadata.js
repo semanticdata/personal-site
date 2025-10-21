33b8af363859b3f4e451aec7c79292f367c0146d
semanticdata <contact@miguelpimentel.do>
2025-10-21 18:50:11 -0500

    new nav style links

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -10,7 +10,27 @@ export default {
     url: "https://miguelpimentel.do/about/",
   },
   social: [
-    { icon: "fas:rss", url: "/feed/feed.xml" },
-    { icon: "fab:github", url: "https://github.com/semanticdata" },
+    {
+      label: "subscribe",
+      icon: "fas:rss",
+      url: "/feed/feed.xml",
+      ariaLabel: "RSS Feed",
+    },
+    {
+      label: "github",
+      icon: "fab:github",
+      url: "https://github.com/semanticdata",
+      ariaLabel: "SemanticData GitHub Profile",
+    },
+  ],
+  footer: [
+    {
+      label: "uses",
+      url: "/uses/",
+    },
+    {
+      label: "meta",
+      url: "/meta/",
+    },
   ],
 };
