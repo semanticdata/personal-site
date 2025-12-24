4d89f7f66e39c60cfaa805c5e8b0975d3d75e389
semanticdata <contact@miguelpimentel.do>
2025-12-24 12:32:23 -0600

    reorganize config

--- eleventy.config.js
+++ eleventy.config.js
@@ -8,17 +8,18 @@ import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import fontAwesomePlugin from "@11ty/font-awesome";
+import markdownItObsidianCallouts from "markdown-it-obsidian-callouts";
+import markdownItFootnote from "markdown-it-footnote";
 
 import pluginFilters from "./src/_config/filters.js";
 import pluginShortcodes from "./src/_config/shortcodes.js";
-import markdownItObsidianCallouts from "markdown-it-obsidian-callouts";
-import markdownItFootnote from "markdown-it-footnote";
 import metadata from "./src/_data/metadata.js";
 
 console.log("Current NODE_ENV:", process.env.NODE_ENV);
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function (eleventyConfig) {
+  // Handle draft posts: Mark title and exclude from production builds
   eleventyConfig.addPreprocessor("drafts", "*", (data) => {
     if (data.draft) {
       data.title = `${data.title} (draft)`;
@@ -28,44 +29,31 @@ export default async function (eleventyConfig) {
     }
   });
 
-  eleventyConfig
-    .addPassthroughCopy({
-      "./public/": "/",
-      "node_modules/@zachleat/snow-fall/snow-fall.js": "assets/js/snow-fall.js",
-    })
-    .addPassthroughCopy("src/feed/simple-rss.xslt")
-    .addPassthroughCopy("src/feed/simple-atom.xslt")
-    .addPassthroughCopy("src/assets/fonts/", "/assets/fonts/")
-    .addPassthroughCopy("src/assets/js/", "/assets/js/");
+  eleventyConfig.addPassthroughCopy({
+    "./public/": "/",
+    "src/feed/simple-rss.xslt": "feed/simple-rss.xslt",
+    "src/feed/simple-atom.xslt": "feed/simple-atom.xslt",
+    "src/assets/fonts/": "assets/fonts/",
+    "src/assets/js/": "assets/js/",
+    "node_modules/@zachleat/snow-fall/snow-fall.js": "assets/js/snow-fall.js",
+  });
 
   eleventyConfig.addWatchTarget("src/assets/css/**/*.css");
   eleventyConfig.addWatchTarget("src/**/*.{svg,webp,png,jpg,jpeg,gif}");
   // eleventyConfig.addWatchTarget("src/**/*.{md,njk,11ty.js}");
 
-  eleventyConfig.addBundle("css", {
-    toFileDirectory: "dist",
-    bundleHtmlContentFromSelector: "style",
-  });
-
-  eleventyConfig.addBundle("js", {
-    toFileDirectory: "dist",
-    bundleHtmlContentFromSelector: "script",
-  });
-
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+  eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
+  eleventyConfig.addPlugin(IdAttributePlugin, {});
+  eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight, {
     preAttributes: { tabindex: 0 },
   });
-  eleventyConfig.addPlugin(pluginNavigation);
-  eleventyConfig.addPlugin(HtmlBasePlugin);
-  eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
-
   eleventyConfig.addPlugin(fontAwesomePlugin, {
     transform: true,
     shortcode: "icon",
   });
-
-  eleventyConfig.addPlugin(pluginRss);
-
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     formats: ["avif", "webp", "auto"],
     widths: ["auto"],
@@ -83,8 +71,17 @@ export default async function (eleventyConfig) {
     urlPath: "/assets/img/",
   });
 
+  eleventyConfig.addBundle("css", {
+    toFileDirectory: "dist",
+    bundleHtmlContentFromSelector: "style",
+  });
+
+  eleventyConfig.addBundle("js", {
+    toFileDirectory: "dist",
+    bundleHtmlContentFromSelector: "script",
+  });
+
   eleventyConfig.addPlugin(pluginFilters);
-  eleventyConfig.addPlugin(IdAttributePlugin, {});
   eleventyConfig.addPlugin(pluginShortcodes(metadata));
 
   eleventyConfig.amendLibrary("md", (mdLib) => {
@@ -95,7 +92,6 @@ export default async function (eleventyConfig) {
 
 export const config = {
   templateFormats: ["md", "njk", "html", "liquid", "11ty.js"],
-
   markdownTemplateEngine: "njk",
   htmlTemplateEngine: "njk",
 
